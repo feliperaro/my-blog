1c54bd0fb7f34d4df27b755349ca00a465019fe2
FehRoque <feliperamosroque@gmail.com>
2024-02-24 13:35:00 -0300

    BlogPost getting title, content and tags

--- src/app/blog-post/page.tsx
+++ src/app/blog-post/page.tsx
@@ -1,33 +1,90 @@
+"use client";
+
+import TagsInput, { Tag } from "@/components/TagsInput";
+import { useState } from "react";
+
+type Post = {
+  title: string;
+  content: string;
+  dateCreated?: Date;
+  category?: string;
+  tags: Tag[];
+};
+
 const BlogPost = () => {
+  const [post, setPost] = useState<Post>({
+    title: "",
+    content: "",
+    tags: [],
+  });
+
+  const handleTitleChange = (title: string) => {
+    setPost({ ...post, title });
+  };
+
+  const handleContentChange = (content: string) => {
+    setPost({ ...post, content });
+  };
+
+  const handleTagChange = (newTags: Tag[]) => {
+    setPost({ ...post, tags: newTags });
+  };
+
+  const handlePost = () => {
+    return;
+  };
+
   return (
     <main className="border border-black flex min-h-screen flex-col justify-between p-12">
       <div className="flex flex-col gap-5">
         <h1 className="font-bold text-center">{"Create your blog post :D"}</h1>
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-2">
           <label className="font-semibold" htmlFor="title">
             {"Title:"}
           </label>
-          <input type="text" name="title" id="title" className="p-2" />
+          <input
+            className="p-2"
+            id="title"
+            name="title"
+            onChange={(e) => handleTitleChange(e.target.value)}
+            type="text"
+            value={post.title}
+          />
         </div>
 
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-2">
           <label className="font-semibold" htmlFor="content">
             {"Content:"}
           </label>
           <textarea
             className="p-2"
-            name="content"
             id="content"
+            name="content"
+            onChange={(e) => handleContentChange(e.target.value)}
             placeholder="Write your content here..."
             required
             rows={10}
+            value={post.content}
           />
         </div>
-        <div className="">
-          <button className="hover:text-gray-500" type="button">
-            {"POST"}
+        <div className="flex justify-center">
+          <button
+            className="
+            border border-gray-700 pb-1 pl-6 pr-6 pt-1 rounded
+            hover:font-bold"
+            onClick={handlePost}
+            type="button"
+          >
+            {"Create blog post"}
           </button>
         </div>
+        <div className="">
+          <TagsInput
+            initialTags={post.tags}
+            onChange={(tags) => handleTagChange(tags)}
+          />
+        </div>
+        <div className="h-fit w-fit">{JSON.stringify(post)}</div>
       </div>
     </main>
   );
