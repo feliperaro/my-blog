b2b67c293da2f72f3dd9133c20d5535f4f2bf7d6
FehRoque <feliperamosroque@gmail.com>
2024-02-26 13:14:00 -0300

    links home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,14 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col gap-2">
-        <h1 className="text-center">BLOG</h1>
+    <main className="flex min-h-screen flex-col gap-10 items-center p-24">
+      <h1 className="font-extrabold text-2xl text-center">BLOG</h1>
+      <div className="flex flex-col gap-1 text-start">
         <h2 className="hover:text-gray-500">
-          <Link href={"/blog-post"}>Create a blog post</Link>
+          <Link href={"/create-post"}>Create</Link>
+        </h2>
+        <h2 className="hover:text-gray-500">
+          <Link href={"/posts"}>Posts</Link>
         </h2>
       </div>
     </main>
