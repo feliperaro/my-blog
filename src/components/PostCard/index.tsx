e78e3586d6d6d1e0aca6ea7e42d994805afe162c
FehRoque <feliperamosroque@gmail.com>
2024-02-29 11:58:42 -0300

    Link and width removed from PostCard

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -1,9 +1,8 @@
 import Post from "@/app/types/post";
-import Link from "next/link";
 
 const PostCard = (props: { post: Post }) => {
   return (
-    <div className="border border-gray-400 flex flex-col min-h-min w-2/5">
+    <div className="border border-gray-400 flex flex-col min-h-min">
       <div className="border-b border-gray-300 font-semibold p-2 text-center">
         {props.post.title}
       </div>
