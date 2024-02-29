c8c507c5c99bf1a35e794025141925122e38ec9b
FehRoque <feliperamosroque@gmail.com>
2024-02-29 16:31:51 -0300

    update texts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col gap-10 items-center p-24">
-      <h1 className="font-extrabold text-2xl text-center">BLOG</h1>
+    <main className="flex min-h-screen flex-col gap-10 p-24">
+      <h1 className="font-bold text-xl">{"Blog Post - Felipe Roque"}</h1>
       <div className="flex flex-col gap-1 text-start">
-        <h2 className="hover:text-gray-500">
-          <Link href={"/create-post"}>Create</Link>
+        <h2 className="hover:font-bold hover:text-gray-800">
+          <Link href={"/create-post"}>{"New post"}</Link>
         </h2>
-        <h2 className="hover:text-gray-500">
-          <Link href={"/posts"}>Posts</Link>
+        <h2 className="hover:font-bold hover:text-gray-800">
+          <Link href={"/posts"}>{"Your posts"}</Link>
         </h2>
       </div>
     </main>
