af863ba24f728067968b9b9dd62b92570c39b3a9
FehRoque <feliperamosroque@gmail.com>
2024-02-29 13:15:24 -0300

    type Post some properties might be undefined

--- src/types/Post/index.ts
+++ src/types/Post/index.ts
@@ -1,11 +1,11 @@
 type Post = {
-  _id: string;
+  _id?: string;
   title: string;
   content: string;
   category?: string;
   tags: string[];
-  created_at: Date;
-  updated_at: Date;
+  created_at?: Date;
+  updated_at?: Date;
 };
 
 export default Post;
