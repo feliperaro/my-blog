b56b2e4ed05f6e6422527c055aaf0255266682ad
FehRoque <feliperamosroque@gmail.com>
2024-02-29 12:32:00 -0300

    props Post | undefined

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -1,13 +1,13 @@
-import Post from "@/app/types/post";
+import Post from "@/types/Post";
 
-const PostCard = (props: { post: Post }) => {
+const PostCard = (props: { post: Post | undefined }) => {
   return (
     <div className="border border-gray-400 flex flex-col min-h-min">
       <div className="border-b border-gray-300 font-semibold p-2 text-center">
-        {props.post.title}
+        {props.post?.title}
       </div>
       <div className="p-2 text-start h-2/3 overflow-hidden text-ellipsis">
-        {props.post.content}
+        {props.post?.content}
       </div>
     </div>
   );
